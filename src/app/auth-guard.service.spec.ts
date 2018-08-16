a385d7abc25261f2ce4fa3a9e0821cfc62e35842
Greg Spitz <gregspitz6@gmail.com>
2018-08-15 21:54:09 -0400

    Add tests for AuthGuardService

--- src/app/auth-guard.service.spec.ts
+++ src/app/auth-guard.service.spec.ts
@@ -1,40 +1,56 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed, inject } from '@angular/core/testing';
-import { Router } from '@angular/router';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { AngularFireAuth } from 'angularfire2/auth';
 
 import { AuthGuardService } from './auth-guard.service';
 import { SecurityService } from './security.service';
 
-const angularFireAuthMock = {
-  
-} as AngularFireAuth;
-
-const securityServiceMock = {
-  
-} as SecurityService;
-
-const routerMock = {
-  
-} as Router;
-
 describe('AuthGuardService', () => {
+  let securityServiceSpy: jasmine.SpyObj<SecurityService>;
+  let routerSpy: jasmine.SpyObj<Router>;
+  let authGuardService: AuthGuardService;
+  const route = new ActivatedRouteSnapshot();
+  const state = {
+    url: "FakeURL"
+  } as RouterStateSnapshot;
+
   beforeEach(() => {
+    const securitySpy = jasmine.createSpyObj("SecurityService", ["authenticated"]);
+    const routeSpy = jasmine.createSpyObj("Router", ["navigate"]);
+
     TestBed.configureTestingModule({
       providers: [
         AuthGuardService,
-        { provide: AngularFireAuth, useValue: angularFireAuthMock },
-        { provide: SecurityService, useValue: securityServiceMock },
-        { provide: Router, useValue: routerMock }
+        { provide: SecurityService, useValue: securitySpy },
+        { provide: Router, useValue: routeSpy }
       ],
       schemas: [
         NO_ERRORS_SCHEMA
       ]
     });
+
+    authGuardService = TestBed.get(AuthGuardService);
+    securityServiceSpy = TestBed.get(SecurityService);
+    routerSpy = TestBed.get(Router);
   });
 
-  it('should be created', inject([AuthGuardService], (service: AuthGuardService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should be created', () => {
+    expect(authGuardService).toBeTruthy();
+  });
+
+  it('should return false and navigate to login on Router on canActivate with authenticated false', () => {
+    securityServiceSpy.authenticated.and.returnValue(false);
+    expect(authGuardService.canActivate(route, state)).toBeFalsy();
+    expect(securityServiceSpy.authenticated).toHaveBeenCalled();
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['login'], jasmine.any(Object));
+  });
+
+  it('should return true on canActivate with authenticated true', () => {
+    securityServiceSpy.authenticated.and.returnValue(true);
+    expect(authGuardService.canActivate(route, state)).toBeTruthy();
+    expect(securityServiceSpy.authenticated).toHaveBeenCalled();
+    expect(routerSpy.navigate).not.toHaveBeenCalled();
+  });
 });
