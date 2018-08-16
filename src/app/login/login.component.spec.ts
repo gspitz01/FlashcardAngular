21d9d934f7da463e02b518f7d8be068062336cd4
Greg Spitz <gregspitz6@gmail.com>
2018-08-15 22:46:57 -0400

    Add tests for LoginComponent

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -5,45 +5,33 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { LoginComponent } from './login.component';
 import { SecurityService } from '../security.service';
 
-const fakeActivatedRoute = {
-  snapshot: {
-    data: {
-      
-    },
-    paramMap: {
-      get: (param: string) => {
-        return "0";
-      }
-    }
-  },
-  queryParams: {
-    subscribe: (func) => {
-      
-    }
-  }
-} as ActivatedRoute;
-
-const securityServiceMock = {
-  
-} as SecurityService;
-
-const routerMock = {
-  
-} as Router;
-
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let securityServiceSpy: jasmine.SpyObj<SecurityService>;
+  let activatedRouteSpy: jasmine.SpyObj<ActivatedRoute>;
+  let routerSpy: jasmine.SpyObj<Router>;
+  const fakeQueryParamsWithReturn = {
+    return: "categories"
+  }
 
   beforeEach(async(() => {
+    const ssSpy = jasmine.createSpyObj("SecurityService", ["login"]);
+    const arSpy = jasmine.createSpyObj("ActivatedRoute", ["queryParams"]);
+    const rSpy = jasmine.createSpyObj("Router", ["navigateByUrl"]);
+    arSpy.queryParams = jasmine.createSpyObj("Observable<Params>", ["subscribe"]);
+    arSpy.queryParams.subscribe.and.callFake(func => {
+        func(fakeQueryParamsWithReturn);
+    });
+
     TestBed.configureTestingModule({
       declarations: [
         LoginComponent
       ],
       providers: [
-        { provide: ActivatedRoute, useValue: fakeActivatedRoute },
-        { provide: SecurityService, useValue: securityServiceMock },
-        { provide: Router, useValue: routerMock }
+        { provide: ActivatedRoute, useValue: arSpy },
+        { provide: SecurityService, useValue: ssSpy },
+        { provide: Router, useValue: rSpy }
       ],
       schemas: [
         NO_ERRORS_SCHEMA
@@ -55,10 +43,27 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    securityServiceSpy = TestBed.get(SecurityService);
+    activatedRouteSpy = TestBed.get(ActivatedRoute);
+    routerSpy = TestBed.get(Router);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create and get return from route.queryParams', () => {
     expect(component).toBeTruthy();
+    expect(component.return).toBe(fakeQueryParamsWithReturn.return);
+    expect(activatedRouteSpy.queryParams.subscribe).toHaveBeenCalledWith(jasmine.any(Function));
+  });
+
+  it('should call login on SecurityService and navigate with router on login', () => {
+    const fauxPromise = {
+      then: (func) => {
+        func();
+      }
+    };
+    securityServiceSpy.login.and.returnValue(fauxPromise);
+    component.login();
+    expect(securityServiceSpy.login).toHaveBeenCalled();
+    expect(routerSpy.navigateByUrl).toHaveBeenCalledWith(fakeQueryParamsWithReturn.return);
   });
 });
